f5a117d0bad96fe1b7080cb1b8da655fc8db29ef
Todd Fleming <tbfleming@gmail.com>
2017-01-04 18:03:07 -0500

    Copy DEFAULTS_GENERIC and CPU_MAP_ATMEGA328P

--- grbl/config.h
+++ grbl/config.h
@@ -34,8 +34,8 @@
 // NOTE: OEMs can avoid the need to maintain/update the defaults.h and cpu_map.h files and use only
 // one configuration file by placing their specific defaults and pin map at the bottom of this file.
 // If doing so, simply comment out these two defines and see instructions below.
-#define DEFAULTS_GENERIC
-#define CPU_MAP_ATMEGA328P // Arduino Uno CPU
+// #define DEFAULTS_GENERIC
+// #define CPU_MAP_ATMEGA328P // Arduino Uno CPU
 
 // Serial baud rate
 // #define BAUD_RATE 230400
@@ -579,8 +579,158 @@
 */
 
 // Paste CPU_MAP definitions here.
+// Define serial port pins and interrupt vectors.
+#define SERIAL_RX     USART_RX_vect
+#define SERIAL_UDRE   USART_UDRE_vect
+
+// Define step pulse output pins. NOTE: All step bit pins must be on the same port.
+#define STEP_DDR        DDRD
+#define STEP_PORT       PORTD
+#define X_STEP_BIT      2  // Uno Digital Pin 2
+#define Y_STEP_BIT      3  // Uno Digital Pin 3
+#define Z_STEP_BIT      4  // Uno Digital Pin 4
+#define STEP_MASK       ((1<<X_STEP_BIT)|(1<<Y_STEP_BIT)|(1<<Z_STEP_BIT)) // All step bits
+
+// Define step direction output pins. NOTE: All direction pins must be on the same port.
+#define DIRECTION_DDR     DDRD
+#define DIRECTION_PORT    PORTD
+#define X_DIRECTION_BIT   5  // Uno Digital Pin 5
+#define Y_DIRECTION_BIT   6  // Uno Digital Pin 6
+#define Z_DIRECTION_BIT   7  // Uno Digital Pin 7
+#define DIRECTION_MASK    ((1<<X_DIRECTION_BIT)|(1<<Y_DIRECTION_BIT)|(1<<Z_DIRECTION_BIT)) // All direction bits
+
+// Define stepper driver enable/disable output pin.
+#define STEPPERS_DISABLE_DDR    DDRB
+#define STEPPERS_DISABLE_PORT   PORTB
+#define STEPPERS_DISABLE_BIT    0  // Uno Digital Pin 8
+#define STEPPERS_DISABLE_MASK   (1<<STEPPERS_DISABLE_BIT)
+
+// Define homing/hard limit switch input pins and limit interrupt vectors.
+// NOTE: All limit bit pins must be on the same port, but not on a port with other input pins (CONTROL).
+#define LIMIT_DDR        DDRB
+#define LIMIT_PIN        PINB
+#define LIMIT_PORT       PORTB
+#define X_LIMIT_BIT      1  // Uno Digital Pin 9
+#define Y_LIMIT_BIT      2  // Uno Digital Pin 10
+#ifdef VARIABLE_SPINDLE // Z Limit pin and spindle enabled swapped to access hardware PWM on Pin 11.
+  #define Z_LIMIT_BIT	   4 // Uno Digital Pin 12
+#else
+  #define Z_LIMIT_BIT    3  // Uno Digital Pin 11
+#endif
+#define LIMIT_MASK       ((1<<X_LIMIT_BIT)|(1<<Y_LIMIT_BIT)|(1<<Z_LIMIT_BIT)) // All limit bits
+#define LIMIT_INT        PCIE0  // Pin change interrupt enable pin
+#define LIMIT_INT_vect   PCINT0_vect
+#define LIMIT_PCMSK      PCMSK0 // Pin change interrupt register
+
+// Define spindle enable and spindle direction output pins.
+#define SPINDLE_ENABLE_DDR    DDRB
+#define SPINDLE_ENABLE_PORT   PORTB
+// Z Limit pin and spindle PWM/enable pin swapped to access hardware PWM on Pin 11.
+#ifdef VARIABLE_SPINDLE
+  #ifdef USE_SPINDLE_DIR_AS_ENABLE_PIN
+    // If enabled, spindle direction pin now used as spindle enable, while PWM remains on D11.
+    #define SPINDLE_ENABLE_BIT    5  // Uno Digital Pin 13 (NOTE: D13 can't be pulled-high input due to LED.)
+  #else
+    #define SPINDLE_ENABLE_BIT    3  // Uno Digital Pin 11
+  #endif
+#else
+  #define SPINDLE_ENABLE_BIT    4  // Uno Digital Pin 12
+#endif
+#ifndef USE_SPINDLE_DIR_AS_ENABLE_PIN
+  #define SPINDLE_DIRECTION_DDR   DDRB
+  #define SPINDLE_DIRECTION_PORT  PORTB
+  #define SPINDLE_DIRECTION_BIT   5  // Uno Digital Pin 13 (NOTE: D13 can't be pulled-high input due to LED.)
+#endif
+
+// Define flood and mist coolant enable output pins.
+#define COOLANT_FLOOD_DDR   DDRC
+#define COOLANT_FLOOD_PORT  PORTC
+#define COOLANT_FLOOD_BIT   3  // Uno Analog Pin 3
+#define COOLANT_MIST_DDR   DDRC
+#define COOLANT_MIST_PORT  PORTC
+#define COOLANT_MIST_BIT   4  // Uno Analog Pin 3
+
+// Define user-control controls (cycle start, reset, feed hold) input pins.
+// NOTE: All CONTROLs pins must be on the same port and not on a port with other input pins (limits).
+#define CONTROL_DDR       DDRC
+#define CONTROL_PIN       PINC
+#define CONTROL_PORT      PORTC
+#define CONTROL_RESET_BIT         0  // Uno Analog Pin 0
+#define CONTROL_FEED_HOLD_BIT     1  // Uno Analog Pin 1
+#define CONTROL_CYCLE_START_BIT   2  // Uno Analog Pin 2
+#define CONTROL_SAFETY_DOOR_BIT   1  // Uno Analog Pin 1 NOTE: Safety door is shared with feed hold. Enabled by config define.
+#define CONTROL_INT       PCIE1  // Pin change interrupt enable pin
+#define CONTROL_INT_vect  PCINT1_vect
+#define CONTROL_PCMSK     PCMSK1 // Pin change interrupt register
+#define CONTROL_MASK      ((1<<CONTROL_RESET_BIT)|(1<<CONTROL_FEED_HOLD_BIT)|(1<<CONTROL_CYCLE_START_BIT)|(1<<CONTROL_SAFETY_DOOR_BIT))
+#define CONTROL_INVERT_MASK   CONTROL_MASK // May be re-defined to only invert certain control pins.
+
+// Define probe switch input pin.
+#define PROBE_DDR       DDRC
+#define PROBE_PIN       PINC
+#define PROBE_PORT      PORTC
+#define PROBE_BIT       5  // Uno Analog Pin 5
+#define PROBE_MASK      (1<<PROBE_BIT)
+
+// Variable spindle configuration below. Do not change unless you know what you are doing.
+// NOTE: Only used when variable spindle is enabled.
+#define SPINDLE_PWM_MAX_VALUE     255 // Don't change. 328p fast PWM mode fixes top value as 255.
+#ifndef SPINDLE_PWM_MIN_VALUE
+  #define SPINDLE_PWM_MIN_VALUE   1   // Must be greater than zero.
+#endif
+#define SPINDLE_PWM_OFF_VALUE     0
+#define SPINDLE_PWM_RANGE         (SPINDLE_PWM_MAX_VALUE-SPINDLE_PWM_MIN_VALUE)
+#define SPINDLE_TCCRA_REGISTER	  TCCR2A
+#define SPINDLE_TCCRB_REGISTER	  TCCR2B
+#define SPINDLE_OCR_REGISTER      OCR2A
+#define SPINDLE_COMB_BIT	        COM2A1
+
+// Prescaled, 8-bit Fast PWM mode.
+#define SPINDLE_TCCRA_INIT_MASK   ((1<<WGM20) | (1<<WGM21))  // Configures fast PWM mode.
+// #define SPINDLE_TCCRB_INIT_MASK   (1<<CS20)               // Disable prescaler -> 62.5kHz
+// #define SPINDLE_TCCRB_INIT_MASK   (1<<CS21)               // 1/8 prescaler -> 7.8kHz (Used in v0.9)
+// #define SPINDLE_TCCRB_INIT_MASK   ((1<<CS21) | (1<<CS20)) // 1/32 prescaler -> 1.96kHz
+#define SPINDLE_TCCRB_INIT_MASK      (1<<CS22)               // 1/64 prescaler -> 0.98kHz (J-tech laser)
+
+// NOTE: On the 328p, these must be the same as the SPINDLE_ENABLE settings.
+#define SPINDLE_PWM_DDR	  DDRB
+#define SPINDLE_PWM_PORT  PORTB
+#define SPINDLE_PWM_BIT	  3    // Uno Digital Pin 11
 
 // Paste default settings definitions here.
-
+#define DEFAULT_X_STEPS_PER_MM 250.0
+#define DEFAULT_Y_STEPS_PER_MM 250.0
+#define DEFAULT_Z_STEPS_PER_MM 250.0
+#define DEFAULT_X_MAX_RATE 500.0 // mm/min
+#define DEFAULT_Y_MAX_RATE 500.0 // mm/min
+#define DEFAULT_Z_MAX_RATE 500.0 // mm/min
+#define DEFAULT_X_ACCELERATION (10.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
+#define DEFAULT_Y_ACCELERATION (10.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
+#define DEFAULT_Z_ACCELERATION (10.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
+#define DEFAULT_X_MAX_TRAVEL 200.0 // mm
+#define DEFAULT_Y_MAX_TRAVEL 200.0 // mm
+#define DEFAULT_Z_MAX_TRAVEL 200.0 // mm
+#define DEFAULT_SPINDLE_RPM_MAX 1000.0 // rpm
+#define DEFAULT_SPINDLE_RPM_MIN 0.0 // rpm
+#define DEFAULT_STEP_PULSE_MICROSECONDS 10
+#define DEFAULT_STEPPING_INVERT_MASK 0
+#define DEFAULT_DIRECTION_INVERT_MASK 0
+#define DEFAULT_STEPPER_IDLE_LOCK_TIME 25 // msec (0-254, 255 keeps steppers enabled)
+#define DEFAULT_STATUS_REPORT_MASK 1 // MPos enabled
+#define DEFAULT_JUNCTION_DEVIATION 0.01 // mm
+#define DEFAULT_ARC_TOLERANCE 0.002 // mm
+#define DEFAULT_REPORT_INCHES 0 // false
+#define DEFAULT_INVERT_ST_ENABLE 0 // false
+#define DEFAULT_INVERT_LIMIT_PINS 0 // false
+#define DEFAULT_SOFT_LIMIT_ENABLE 0 // false
+#define DEFAULT_HARD_LIMIT_ENABLE 0  // false
+#define DEFAULT_INVERT_PROBE_PIN 0 // false
+#define DEFAULT_LASER_MODE 0 // false
+#define DEFAULT_HOMING_ENABLE 0  // false
+#define DEFAULT_HOMING_DIR_MASK 0 // move positive dir
+#define DEFAULT_HOMING_FEED_RATE 25.0 // mm/min
+#define DEFAULT_HOMING_SEEK_RATE 500.0 // mm/min
+#define DEFAULT_HOMING_DEBOUNCE_DELAY 250 // msec (0-65k)
+#define DEFAULT_HOMING_PULLOFF 1.0 // mm
 
 #endif
