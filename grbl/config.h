063b1f6748796076e6fa936b085c19135a5454f5
cprezzi <cprezzi@comsulting.ch>
2017-05-10 17:16:17 +0200

    Config for K40

--- grbl/config.h
+++ grbl/config.h
@@ -103,12 +103,12 @@
 // on separate pin, but homed in one cycle. Also, it should be noted that the function of hard limits
 // will not be affected by pin sharing.
 // NOTE: Defaults are set for a traditional 3-axis CNC machine. Z-axis first to clear, followed by X & Y.
-#define HOMING_CYCLE_0 (1<<Z_AXIS)                // REQUIRED: First move Z to clear workspace.
-#define HOMING_CYCLE_1 ((1<<X_AXIS)|(1<<Y_AXIS))  // OPTIONAL: Then move X,Y at the same time.
+//#define HOMING_CYCLE_0 (1<<Z_AXIS)                // REQUIRED: First move Z to clear workspace.
+//#define HOMING_CYCLE_1 ((1<<X_AXIS)|(1<<Y_AXIS))  // OPTIONAL: Then move X,Y at the same time.
 // #define HOMING_CYCLE_2                         // OPTIONAL: Uncomment and add axes mask to enable
 
 // NOTE: The following are two examples to setup homing for 2-axis machines.
-// #define HOMING_CYCLE_0 ((1<<X_AXIS)|(1<<Y_AXIS))  // NOT COMPATIBLE WITH COREXY: Homes both X-Y in one cycle. 
+#define HOMING_CYCLE_0 ((1<<X_AXIS)|(1<<Y_AXIS))  // NOT COMPATIBLE WITH COREXY: Homes both X-Y in one cycle. 
 
 // #define HOMING_CYCLE_0 (1<<X_AXIS)  // COREXY COMPATIBLE: First home X
 // #define HOMING_CYCLE_1 (1<<Y_AXIS)  // COREXY COMPATIBLE: Then home Y
@@ -359,10 +359,10 @@
 //      pwm = scaled value. settings.rpm_min scales to SPINDLE_PWM_MIN_VALUE. settings.rpm_max
 //            scales to SPINDLE_PWM_MAX_VALUE.
 
-#define SPINDLE_PWM_PERIOD        (SystemCoreClock / 40000)         // SystemCoreClock / frequency
-#define SPINDLE_PWM_OFF_VALUE     (SPINDLE_PWM_PERIOD * 0.0)    // SPINDLE_PWM_PERIOD * fraction
-#define SPINDLE_PWM_MIN_VALUE     (SPINDLE_PWM_PERIOD * 0.0)    // SPINDLE_PWM_PERIOD * fraction
-#define SPINDLE_PWM_MAX_VALUE     (SPINDLE_PWM_PERIOD * 1.0)  // SPINDLE_PWM_PERIOD * fraction
+//#define SPINDLE_PWM_PERIOD        (SystemCoreClock / 40000)         // SystemCoreClock / frequency
+#define SPINDLE_PWM_OFF_VALUE     0.0    // SPINDLE_PWM_PERIOD * fraction
+//#define SPINDLE_PWM_MIN_VALUE     (SPINDLE_PWM_PERIOD * 0.0)    // SPINDLE_PWM_PERIOD * fraction
+//#define SPINDLE_PWM_MAX_VALUE     (SPINDLE_PWM_PERIOD * 1.0)  // SPINDLE_PWM_PERIOD * fraction
 
 // Used by variable spindle output only. This forces the PWM output to a minimum duty cycle when enabled.
 // The PWM pin will still read 0V when the spindle is disabled. Most users will not need this option, but
@@ -663,10 +663,10 @@
 #define LIMIT_DDR         LPC_GPIO1->FIODIR
 #define LIMIT_PIN         LPC_GPIO1->FIOPIN
 #define LIMIT_PORT        LPC_GPIO1->FIOPIN
-#define X_LIMIT_BIT       25  // X-MIN=24, X-MAX=25
+#define X_LIMIT_BIT       24  // X-MIN=24, X-MAX=25
 #define Y_LIMIT_BIT       27  // Y-MIN=26, Y-MAX=27
 #define Z_LIMIT_BIT	      29  // Z-MIN=28, Z-MAX=29
-#define LIMIT_MASK       ((1<<X_LIMIT_BIT)|(1<<Y_LIMIT_BIT)|(1<<Z_LIMIT_BIT)) // All limit bits
+#define LIMIT_MASK       ((1<<X_LIMIT_BIT)|(1<<Y_LIMIT_BIT) //|(1<<Z_LIMIT_BIT)) // All limit bits
 // hard limits not ported    #define LIMIT_INT        PCIE0  // Pin change interrupt enable pin
 // hard limits not ported    #define LIMIT_INT_vect   PCINT0_vect
 // hard limits not ported    #define LIMIT_PCMSK      PCMSK0 // Pin change interrupt register
@@ -728,53 +728,54 @@
 // PWM Channel      PWM1_CH1  PWM1_CH2  PWM1_CH3  PWM1_CH4  PWM1_CH5  PWM1_CH6
 // Primary pin      P1.18     P1.20     P1.21     P1.23     P1.24     P1.26
 // Secondary pin    P2.0      P2.1      P2.2      P2.3      P2.4      P2.5
-#define SPINDLE_PWM_CHANNEL           PWM1_CH5
+#define SPINDLE_PWM_CHANNEL           PWM1_CH6
 #define SPINDLE_PWM_USE_PRIMARY_PIN   false
 #define SPINDLE_PWM_USE_SECONDARY_PIN true
 
 // Stepper current control
-#define CURRENT_I2C Driver_I2C1         // I2C driver for current control. Comment out to disable.
+//#define CURRENT_I2C Driver_I2C1         // I2C driver for current control. Comment out to disable.
 #define CURRENT_MCP44XX_ADDR 0b0101100  // Address of MCP44XX
 #define CURRENT_WIPERS {0, 1, 6, 7};    // Wiper registers (X, Y, Z, A)
 #define CURRENT_FACTOR 113.33           // Convert amps to digipot value
 
 // Paste default settings definitions here.
-#define DEFAULT_X_STEPS_PER_MM 158.0
-#define DEFAULT_Y_STEPS_PER_MM 158.0
-#define DEFAULT_Z_STEPS_PER_MM 158.0
-#define DEFAULT_X_MAX_RATE 30000 // mm/min
-#define DEFAULT_Y_MAX_RATE 30000 // mm/min
-#define DEFAULT_Z_MAX_RATE 500.0 // mm/min
-#define DEFAULT_X_ACCELERATION (5000.0*60*60) // 5000*60*60 mm/min^2 = 5000 mm/sec^2
-#define DEFAULT_Y_ACCELERATION (5000.0*60*60) // 5000*60*60 mm/min^2 = 5000 mm/sec^2
-#define DEFAULT_Z_ACCELERATION (5000.0*60*60) // 5000*60*60 mm/min^2 = 5000 mm/sec^2
+#define DEFAULT_X_STEPS_PER_MM 160.0
+#define DEFAULT_Y_STEPS_PER_MM 160.0
+#define DEFAULT_Z_STEPS_PER_MM 160.0
+#define DEFAULT_X_MAX_RATE 24000 // mm/min
+#define DEFAULT_Y_MAX_RATE 24000 // mm/min
+#define DEFAULT_Z_MAX_RATE 24000 // mm/min
+#define DEFAULT_X_ACCELERATION (2500.0*60*60) // 5000*60*60 mm/min^2 = 5000 mm/sec^2
+#define DEFAULT_Y_ACCELERATION (2500.0*60*60) // 5000*60*60 mm/min^2 = 5000 mm/sec^2
+#define DEFAULT_Z_ACCELERATION (2500.0*60*60) // 5000*60*60 mm/min^2 = 5000 mm/sec^2
 #define DEFAULT_X_CURRENT 0.0 // amps
 #define DEFAULT_Y_CURRENT 0.0 // amps
 #define DEFAULT_Z_CURRENT 0.0 // amps
 #define DEFAULT_A_CURRENT 0.0  // amps
-#define DEFAULT_X_MAX_TRAVEL 200.0 // mm
+#define DEFAULT_X_MAX_TRAVEL 300.0 // mm
 #define DEFAULT_Y_MAX_TRAVEL 200.0 // mm
-#define DEFAULT_Z_MAX_TRAVEL 200.0 // mm
+#define DEFAULT_Z_MAX_TRAVEL 50.0 // mm
+#define DEFAULT_SPINDLE_PWM_FREQ 5000 // Hz
 #define DEFAULT_SPINDLE_RPM_MAX 1.0 // rpm
 #define DEFAULT_SPINDLE_RPM_MIN 0.0 // rpm
 #define DEFAULT_STEP_PULSE_MICROSECONDS 1
 #define DEFAULT_STEPPING_INVERT_MASK 0
 #define DEFAULT_DIRECTION_INVERT_MASK 0
 #define DEFAULT_STEPPER_IDLE_LOCK_TIME 25 // msec (0-254, 255 keeps steppers enabled)
-#define DEFAULT_STATUS_REPORT_MASK 1 // MPos enabled
+#define DEFAULT_STATUS_REPORT_MASK 0 // WPos enabled
 #define DEFAULT_JUNCTION_DEVIATION 0.01 // mm
 #define DEFAULT_ARC_TOLERANCE 0.002 // mm
 #define DEFAULT_REPORT_INCHES 0 // false
 #define DEFAULT_INVERT_ST_ENABLE 0 // false
-#define DEFAULT_INVERT_LIMIT_PINS 0 // false
+#define DEFAULT_INVERT_LIMIT_PINS 1 // false
 #define DEFAULT_SOFT_LIMIT_ENABLE 0 // false
 #define DEFAULT_HARD_LIMIT_ENABLE 0  // false
 #define DEFAULT_INVERT_PROBE_PIN 0 // false
 #define DEFAULT_LASER_MODE 1 // true
 #define DEFAULT_HOMING_ENABLE 0  // false
 #define DEFAULT_HOMING_DIR_MASK 0 // move positive dir
-#define DEFAULT_HOMING_FEED_RATE 25.0 // mm/min
-#define DEFAULT_HOMING_SEEK_RATE 500.0 // mm/min
+#define DEFAULT_HOMING_FEED_RATE 50.0 // mm/min
+#define DEFAULT_HOMING_SEEK_RATE 6000.0 // mm/min
 #define DEFAULT_HOMING_DEBOUNCE_DELAY 250 // msec (0-65k)
 #define DEFAULT_HOMING_PULLOFF 1.0 // mm
 
